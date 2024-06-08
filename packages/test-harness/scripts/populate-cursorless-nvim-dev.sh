d919a7a00b9bc3f502e5c0b201961c1d9c49ee93
Cedric Halbronn <387346+saidelike@users.noreply.github.com>
2024-06-08 13:48:32 +0100

    Delete packages/test-harness/scripts/populate-cursorless-nvim-dev.sh

--- packages/test-harness/scripts/populate-cursorless-nvim-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# TODO: Actually use this script locally
-
-out_dir=../../dist/cursorless.nvim
-mkdir -p "$out_dir/node"
-
-ln -sf "$(pwd)" "$out_dir/node/test-harness"
